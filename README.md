f0218fd4a611844a5026c46da2ba6f2f762a099c
wyattbrashear <bfamily610@icloud.com>
2026-06-06 07:45:47 -0700

    I am beeg brain

---
+++ README.md
@@ -0,0 +1,2 @@
+# Automated Propelled Water Dispensory System (Water Turret)
+## A little side project while i'm working on MissTEA
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# Water Turret
+# Water Turret, Uses AI for targeting and whatnot
+from ultralytics import YOLO
+import cv2
+import argparse
+# Calculate the 9 sectors
+# Ok so basic explanation of how this thing works, it splits the screen into 9 sectors and adjusts its targeting until
+# the target's center is in the center of the screen
+
+def calculate_sectors(camera_x, camera_y):
+    sector_width = camera_x // 3
+    sector_height = camera_y // 3
+    sectors = []
+    for y in range(3):
+        for x in range(3):
+            #Each sector should be a pair 2 cordinates
+            sector_tmpvar = [sector_width*x, sector_height*y]
+            sectors.append([[],[]])
